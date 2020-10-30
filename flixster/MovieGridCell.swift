9abda45b7adc7ce1d1c5d052c156d42f9376207d
matt-day <mday.mid@gmail.com>
2020-10-30 17:28:58 -0400

    part 2 of Flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Matt Day on 10/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
